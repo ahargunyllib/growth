6c80d4d655a86cac1b9840d93b9dd34723a392e8
ahargunyllib <billy.bpm03@gmail.com>
2025-10-27 02:00:35 +0700

    feat: Add RepositoryModule for DI
    Introduces a new Dagger Hilt module, `RepositoryModule`, to provide repository dependencies.
    
    - Creates `RepositoryModule.kt` installed in `SingletonComponent`.
    - Binds the `AuthRepository` interface to its implementation, `AuthRepositoryImpl`, as a singleton.

---
+++ app/src/main/java/com/ahargunyllib/growth/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.ahargunyllib.growth.di
+
+import com.ahargunyllib.growth.contract.AuthRepository
+import com.ahargunyllib.growth.repository.AuthRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
